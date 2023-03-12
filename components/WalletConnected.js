a1ae88aa258be581a9d49dcfc0b1a7da861ed288
Rukmani <101476931+rukmani887799@users.noreply.github.com>
2023-03-12 20:50:20 +0530

    Update WalletConnected.js

--- components/WalletConnected.js
+++ components/WalletConnected.js
@@ -2,40 +2,32 @@ import React from "react";
 import useBlockChain from "../hooks/useBlockChain";
 import { providers, Contract, ethers, utils } from "ethers";
 import { Typography, Grid, Link, Button } from "@material-ui/core";
-import Onboard from "@web3-onboard/core";
-import argentModule from "@web3-onboard/argent";
+import { getEthereumProvider } from "@argent/login";
+
 
 const WalletConnected = () => {
   const { connectedWallet } = useBlockChain();
   console.log(connectedWallet);
-  const argent = argentModule();
-
-const onboard = Onboard({
-  // ... other Onboard options
-  wallets: [
-    argent,
-    // ... other wallets
-  ],
-  chains: [
-    {
-      id: "0x118",  // = 280
-      token: "ETH",
-      label: "zkSync Goerli",
+  
+ async function Connect() {
+    const ethereumProvider = await getEthereumProvider({
+      chainId: 280,
       rpcUrl: "https://zksync2-testnet.zksync.dev",
-    },
-    // ... other chains
-  ],
-  appMetadata: {
-    name: "zk sync safe",
-    icon: "https://user-images.githubusercontent.com/101476931/224553007-1d6a9a43-8834-47a5-a578-9b763d91259e.jpeg",
-    description: "A multi sig wallet on the zk sync era testnet",
+      walletConnect: {
+        metadata: {
+          name: "zk sync Safe",
+          description: "A multi sig wallet on the zk sync era testnet.",
+          url: "https://zksync-safe.vercel.app/",
+          icons: [
+            "https://user-images.githubusercontent.com/101476931/224553007-1d6a9a43-8834-47a5-a578-9b763d91259e.jpeg",
+          ],
+        },
+      },
+    });
+    await ethereumProvider.enable();
   }
-});
 
-const Connect = async () => {
-const connectedWallets = await onboard.connectWallet();
-console.log(connectedWallets);
-};
+
 
   return (
     <div>
