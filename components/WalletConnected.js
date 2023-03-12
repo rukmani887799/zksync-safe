1c8ea139150baebf43aefe97a78f9ca6a92b3c76
rukmani887799 <101476931+rukmani887799@users.noreply.github.com>
2023-03-12 19:51:10 +0530

    feat>> DONE

---
+++ components/WalletConnected.js
@@ -0,0 +1,100 @@
+import React from "react";
+import useBlockChain from "../hooks/useBlockChain";
+import { providers, Contract, ethers, utils } from "ethers";
+import { Typography, Grid, Link, Button } from "@material-ui/core";
+import Onboard from "@web3-onboard/core";
+import argentModule from "@web3-onboard/argent";
+
+const WalletConnected = () => {
+  const { connectedWallet } = useBlockChain();
+  console.log(connectedWallet);
+  const argent = argentModule();
+
+const onboard = Onboard({
+  // ... other Onboard options
+  wallets: [
+    argent,
+    // ... other wallets
+  ],
+  chains: [
+    {
+      id: "0x118",  // = 280
+      token: "ETH",
+      label: "zkSync Goerli",
+      rpcUrl: "https://zksync2-testnet.zksync.dev",
+    },
+    // ... other chains
+  ]
+});
+
+const Connect = async () => {
+const connectedWallets = await onboard.connectWallet();
+console.log(connectedWallets);
+};
+
+  return (
+    <div>
+      {" "}
+      <Grid>
+        {connectedWallet ? (
+          <>
+            <Typography
+              variant="body1"
+              align="center"
+              gutterBottom
+              component="div"
+            >
+              {`Metamask connected account: ${connectedWallet.substring(
+                0,
+                5
+              )}...${connectedWallet.slice(-4)}`}
+            </Typography>
+            <Typography
+              variant="body1"
+              align="center"
+              gutterBottom
+              component="div"
+            >
+              {" "}
+              <Link
+                href="https://goerli.explorer.zksync.io/address/0x44746F3f7D503f840CbB2e6Ef82FB0d75EFa9A53"
+                variant="body1"
+                align="center"
+                underline="hover"
+                target="_blank"
+              >
+                Verified Contract on zk-sync ✅
+              </Link>
+            </Typography>
+            <br></br>
+            <Typography
+              variant="body1"
+              align="center"
+              gutterBottom
+              component="div"
+            >Wanted to use Argent Wallet <br></br>
+            <Button component="div" align="center" variant="contained" color="primary" onClick={Connect}>
+            Connect Argent Wallet
+            </Button>
+            </Typography>
+          </>
+        ) : (
+          <>
+          <Typography
+              variant="body1"
+              align="center"
+              gutterBottom
+              component="div"
+            >Wanted to use Argent Wallet<br></br>
+            <Button  align="center" variant="contained" color="primary" onClick={Connect}>
+              Connect Argent Wallet
+            </Button>
+            </Typography>
+            </>
+        )}
+      </Grid>
+    </div>
+  );
+};
+
+export default WalletConnected;
