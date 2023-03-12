c09d68698bf0c4098323780472fd11de27d0295c
Rukmani <101476931+rukmani887799@users.noreply.github.com>
2023-03-12 20:36:52 +0530

    Update WalletConnected.js

--- components/WalletConnected.js
+++ components/WalletConnected.js
@@ -25,7 +25,7 @@ const onboard = Onboard({
     },
     // ... other chains
   ],
-  appMetadata: {
+  metadata: {
     name: "zk sync safe",
     icon: "https://user-images.githubusercontent.com/101476931/224553007-1d6a9a43-8834-47a5-a578-9b763d91259e.jpeg",
     description: "A multi sig wallet on the zk sync era testnet",
